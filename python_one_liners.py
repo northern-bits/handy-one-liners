4c8972ccb8426253e2e11270acd31c6b7f13b04c
rdgl <53665705+rdgl@users.noreply.github.com>
2019-11-29 12:54:52 -0500

    Update python_one_liners.py

--- python_one_liners.py
+++ python_one_liners.py
@@ -1,4 +1,5 @@
 # N factorial one liner. example( 3 factorial is 0^2+1^2+2^2+3^2=14)
 print(sum([x ** 2 for x in range(N + 1)]))
 # Generate N letters of the alphabet in uppercase
+imort string
 print([x for x in (string.ascii_uppercase[:N])])
